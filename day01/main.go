2573c1a0f7ae8bc32cb4512ec95ce4ec436b08d3
afonsorc <afonsorc@outlook.com>
2023-12-02 14:00:08 +0000

    clean: main function

--- day01/main.go
+++ day01/main.go
@@ -8,34 +8,23 @@ import (
 	"unicode"
 )
 
-func readInputFile(path string) ([]string, error) {
-	file, err := os.Open(path)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	var lines []string
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-	return lines, scanner.Err()
-}
-
 func main() {
 
-	input, err := readInputFile("input.txt")
-	if err != nil {
-		fmt.Println("Error: Failed to read input file")
-		os.Exit(1)
+	file, _ := os.Open("input.txt")
+	defer file.Close()
+
+	var input []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		input = append(input, scanner.Text())
 	}
 
 	solution1 := solve(input, false)
+	solution2 := solve(input, true)
+
 	fmt.Print("Part 1: ")
 	fmt.Println(solution1)
 
-	solution2 := solve(input, true)
 	fmt.Print("Part 2: ")
 	fmt.Println(solution2)
 }
--- day02/main.go
+++ day02/main.go
@@ -8,27 +8,15 @@ import (
 	"strings"
 )
 
-func readInputFile(path string) ([]string, error) {
-	file, err := os.Open(path)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	var lines []string
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-	return lines, scanner.Err()
-}
-
 func main() {
 
-	input, err := readInputFile("input.txt")
-	if err != nil {
-		fmt.Println("Error: Failed to read input file")
-		os.Exit(1)
+	file, _ := os.Open("input.txt")
+	defer file.Close()
+
+	var input []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		input = append(input, scanner.Text())
 	}
 
 	solution1 := solve(input, false)
@@ -54,20 +42,17 @@ func solve(input []string, isPartTwo bool) any {
 
 	for _, line := range input {
 
+		game := strings.Split(line, " ")
 		validGame := true
 		if isPartTwo {
 			clear(cubes)
 		}
-		game := strings.Split(line, " ")
-		for i := 2; i < len(game); i += 2 {
 
-			cubeAmount, _ := strconv.Atoi(game[i])
-			if cubeAmount > cubes[game[i+1]] {
+		for i := 2; i < len(game); i += 2 {
+			if cubeAmount, _ := strconv.Atoi(game[i]); cubeAmount > cubes[game[i+1]] {
+				validGame = false
 				if isPartTwo {
 					cubes[game[i+1]] = cubeAmount
-				} else {
-					validGame = false
-					break
 				}
 			}
 		}
